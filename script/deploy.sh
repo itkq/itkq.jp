1fb2e49ea0a1c562a364e7b1dba3cb2a1b6fabde
itkq <kuogsi@gmail.com>
2017-04-30 21:34:35 +0900

    Oops

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 hugo -t sustain
-git add public
 git rev-parse HEAD > public/revision/index.html
+git add public
 LANG=C git commit -m "[ci skip] rebuild at $(date)"
 git subtree split --branch gh-pages --prefix public
 git push -f origin gh-pages:gh-pages
