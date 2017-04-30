26c7a8209088354b858626751f4a4e2c20124e5b
itkq <kuogsi@gmail.com>
2017-04-30 20:50:04 +0900

    Add revision api

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,7 @@
 
 hugo -t sustain
 git add public
+git rev-parse HEAD > public/revision/index.html
 LANG=C git commit -m "[ci skip] rebuild at $(date)"
 git subtree split --branch gh-pages --prefix public
 git push -f origin gh-pages:gh-pages
