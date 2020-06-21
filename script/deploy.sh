ae4f0b3ed1163174bb6ef2fe7cc542b8acc1cff7
Takuya Kosugiyama <re@itkq.jp>
2020-06-21 19:04:29 +0900

    Remove deploy script

--- script/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hugo
-mkdir -p public/revision
-git rev-parse HEAD > public/revision/index.html
-git add public -f
-LANG=C git commit -m "[ci skip] rebuild at $(date)"
-git subtree split --branch gh-pages --prefix public
-git push -f origin gh-pages:gh-pages
